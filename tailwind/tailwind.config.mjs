1c4837c8979cb633fc297c561f5afed29e9f9ce3
RampantDespair <rampantdespair@gmail.com>
2025-02-05 14:17:35 -0500

    Added tailwind config

---
+++ tailwind/tailwind.config.mjs
@@ -0,0 +1,17 @@
+/**
+ * @type {import("tailwindcss").Config}
+ * */
+const config = {
+  content: ["./{app,components,pages,src}/**/*.{js,jsx,mdx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
