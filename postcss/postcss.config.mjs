d861fc7fe995f8f924603d3ad91ac6f5c342fae2
RampantDespair <rampantdespair@gmail.com>
2025-02-05 14:26:58 -0500

    Added postcss config

---
+++ postcss/postcss.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import("postcss-load-config").Config} */
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    "postcss-flexbugs-fixes": {},
+    "postcss-preset-env": {
+      autoprefixer: {
+        flexbox: "no-2009",
+        //grid: true,
+      },
+      stage: 3,
+      features: {
+        "custom-properties": false,
+      },
+    },
+    "postcss-preset-mantine": {},
+    "postcss-simple-vars": {
+      variables: {
+        "mantine-breakpoint-xs": "36em",
+        "mantine-breakpoint-sm": "48em",
+        "mantine-breakpoint-md": "62em",
+        "mantine-breakpoint-lg": "75em",
+        "mantine-breakpoint-xl": "88em",
+      },
+    },
+  },
+};
+
+export default config;
